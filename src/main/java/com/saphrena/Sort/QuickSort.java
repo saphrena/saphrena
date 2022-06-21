f7dbb0477c69efd3eee78706b8758cfce40bf139
saphrena <1422647137@>
2022-06-21 23:53:04 +0800

    快排

--- src/main/java/com/saphrena/Sort/QuickSort.java
+++ src/main/java/com/saphrena/Sort/QuickSort.java
@@ -6,36 +6,38 @@ import java.util.Arrays;
  * @author 热河
  * @created 2022/5/27 16:22
  * @project saphrena
+ * 1.先取出数列中的第一个数作为基准数
+ * 2.将数列中比基准数大的数全部放在它的右边，比准数小的数全部放在它的左边
+ * 3.然后在对左右两部分重复第二步，直到各区间只有一个数
  * @Description
  */
 public class QuickSort {
-    public static int[] resolve(int[] arr, int start, int end) {
-        int pivot = arr[start];
-        int i = start;
-        int j = end;
-        while (i<j) {
-            while ((i<j)&&(arr[j]>pivot)) {
-                j--;
-            }
-            while ((i<j)&&(arr[i]<pivot)) {
-                i++;
-            }
-            if ((arr[i]==arr[j])&&(i<j)) {
-                i++;
-            } else {
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
+    public static void resolve(int[] array, int left, int right) {
+        //left数组的最小下标，right数组最大下标
+        if (left > right) {                    //判断本次是否执行
+            return;
         }
-        if (i-1>start) arr=resolve(arr,start,i-1);
-        if (j+1<end) arr=resolve(arr,j+1,end);
-        return (arr);
+        int low = left;
+        int high=right;
+        int mid=array[low];                    //将数组第一个数定为基准
+
+        while(low<high){
+            while(low<high && array[high]>=mid) //从后往前，直到找到小于基准的数
+                high--;
+            array[low]=array[high];            //小数移动到前面
+            while(low<high && array[low]<=mid)  //从前往后，直到找到大于基准的数
+                low++;
+            array[high]=array[low];            //大数移动到后面
+        }
+
+        array[low]=mid;                        //此时low在中间，将基准放入
+        resolve(array,left,low-1);      //左数组递归
+        resolve(array,low+1,right);      //右数组递归
     }
 
     public static void main(String[] args) {
-        int[] arr = {1,3,5,7,2};
-        int[] result = resolve(arr, 0, arr.length - 1);
-        System.out.println(Arrays.toString(result));
+        int[] arr = {6, 9, 6, 6, 8, 7, 10, 5, 1, 6};
+        resolve(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
     }
 }

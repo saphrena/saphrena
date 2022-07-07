b798f06cd6b346675ef27240b70b041b938e0ac6
saphrena <1422647137@>
2022-07-07 23:44:12 +0800

    二叉树

---
+++ src/main/java/com/saphrena/Java/TreeNode.java
@@ -0,0 +1,27 @@
+package com.saphrena.Java;
+
+/**
+ * @author saphrena
+ * @description 二叉树
+ * @create 2022/7/7 007 23:42
+ */
+
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode() {
+    }
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}

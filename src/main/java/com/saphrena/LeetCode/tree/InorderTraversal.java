71dbce3befe2e447d7de528d123a3f5a71395430
saphrena <1422647137@>
2022-07-15 05:05:10 +0800

    git

--- src/main/java/com/saphrena/LeetCode/tree/InorderTraversal.java
+++ src/main/java/com/saphrena/LeetCode/tree/InorderTraversal.java
@@ -12,7 +12,7 @@ import java.util.List;
  *
  * 递归
  * 左---根---右
- * 定义 inorder(root) 表示当前遍历到}root 节点的答案，那么按照定义，
+ * 定义 inorder(root) 表示当前遍历到root 节点的答案，那么按照定义，
  * 我们只要递归调用 inorder(root.left) 来遍历root 节点的左子树，
  * 然后将 root 节点的值加入答案，再递归调用inorder(root.right)
  * 来遍历root 节点的右子树即可，递归终止的条件为碰到空节点

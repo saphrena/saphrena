cf4724d139a823295e7a257bf998a85c5f1f3fe5
saphrena <1422647137@>
2022-07-15 04:37:38 +0800

    二叉树的最大深度

---
+++ src/main/java/com/saphrena/LeetCode/MaxDepth.java
@@ -0,0 +1,30 @@
+package com.saphrena.LeetCode;
+
+import com.saphrena.Java.TreeNode;
+
+/**
+ * @author saphrena
+ * @description maxDepth
+ * @create 2022/7/15 015 4:33
+ *
+ * 如果我们知道了左子树和右子树的最大深度 ll 和 rr，那么该二叉树的最大深度即为max(l,r)+1
+ */
+
+
+public class MaxDepth {
+    public int maxDepth(TreeNode root) {
+//        终止条件：当前节点为空
+//        找出返回值：节点为空时说明高度为 0，所以返回 0
+        if(root == null) {
+            return 0;
+        } else {
+            int left = maxDepth(root.left);
+            int right = maxDepth(root.right);
+            return Math.max(left, right) + 1;
+        }
+    }
+
+    public int maxDepth1(TreeNode root) {
+        return root == null ? 0 : Math.max(this.maxDepth(root.left), this.maxDepth(root.right)) + 1;
+    }
+}
